76b2e7abf7848b322d45b81069582700a3869c4a
SHEKHARSR <shekharsraikar@gmail.com>
2021-06-06 14:44:28 +0200

    cleanup

--- src/svea_core/src/svea/controllers/tamp_control.py
+++ src/svea_core/src/svea/controllers/tamp_control.py
@@ -37,31 +37,3 @@ class TAMPController(object):
         steering = self.recieved_steering #self.tamp_control.steering
         velocity = self.recieved_velocity #self.tamp_control.velocity
         return steering,velocity         
-    def find_target(self, state):
-        ind = self._calc_target_index(state)
-        tx = self.traj_x[ind]
-        ty = self.traj_y[ind]
-        self.target = (tx, ty)
-
-    def _calc_target_index(self, state):
-        # search nearest point index
-        dx = [state.x - icx for icx in self.traj_x]
-        dy = [state.y - icy for icy in self.traj_y]
-        d = [abs(math.sqrt(idx ** 2 + idy ** 2)) for (idx, idy) in zip(dx, dy)]
-        ind = d.index(min(d))
-        dist = 0.0
-        Lf = self.k * state.v + self.Lfc
-
-        # search look ahead target point index
-        while Lf > dist and (ind + 1) < len(self.traj_x):
-            dx = self.traj_x[ind + 1] - self.traj_x[ind]
-            dy = self.traj_y[ind + 1] - self.traj_y[ind]
-            dist += math.sqrt(dx ** 2 + dy ** 2)
-            ind += 1
-
-        # terminating condition       
-        if self.target != None:
-            # Calculate difference between target and actual state
-            if(math.hypot(self.traj_x[-1]-state.x, self.traj_y[-1]-state.y) < 0.1):
-                self.is_finished = True
-        return ind
--- src/svea_core/src/svea/svea_managers/tamp_svea.py
+++ src/svea_core/src/svea/svea_managers/tamp_svea.py
@@ -7,18 +7,18 @@ import rospy
 from math import cos, sin, sqrt
 
 from svea_archetypes import SVEAManager
-from path_following_sveas import SVEAPurePursuit
+#from path_following_sveas import SVEAPurePursuit
 from svea.data import BasicDataHandler, TrajDataHandler, RVIZPathHandler
 
 class TAMP_svea_manager(SVEAManager):
     """ Container for TAMP_SVEA"""
 
-    def __init__(self, vehicle_name, localizer, controller,traj_x, traj_y, data_handler = RVIZPathHandler):
+    def __init__(self, vehicle_name, localizer, controller, data_handler = RVIZPathHandler):
         SVEAManager.__init__(self, vehicle_name, localizer, controller,
                                    data_handler=data_handler)
 
         
-        self.update_traj(traj_x, traj_y)
+        #self.update_traj(traj_x, traj_y)
 
         #goto
         self.goto_thresh = 0.05  # m
@@ -37,7 +37,7 @@ class TAMP_svea_manager(SVEAManager):
                       used instead, defaults to None
         :type state: VehicleState, or None
 
-        :return: Computed steering and velocity inputs from pure-pursuit
+        :return: Computed steering and velocity inputs from TAMP from selected control
                  algorithm
         :rtype: float, float
         """
@@ -48,48 +48,7 @@ class TAMP_svea_manager(SVEAManager):
             steering, velocity = self.controller.get_control()
             self.data_handler.update_target(self.controller.target)
         return steering, velocity
-    
-
-    def goto_pt(self, pt):
-        """Compute control to go to single point, taking advantage of
-        the pure-pursuit controller
-
-        :param pt: Point to go to
-        :type pt: tuple
-        :return: Computed steering and velocity inputs from pure-pursuit
-                 algorithm
-        :rtype: float, float
-        """
-        curr_xy = [self.state.x, self.state.y]
-        target_xy = (pt[0], pt[1])
-        dist = math.sqrt((curr_xy[0] - target_xy[0])**2
-                         + (curr_xy[1] - target_xy[1])**2)
-
-        if dist > self.goto_thresh:
-            steering, velocity = \
-                self.controller.compute_control(self.state, target_xy)
-            self.data_handler.update_target(self.controller.target)
-            return steering, velocity
-        else:
-            steering = 0.0
-            velocity = 0.0
-            return steering, velocity
-
-    def update_traj(self, traj_x, traj_y):
-        """Update trajectory
-
-        :param traj_x: X coordinates of trajectory, defaults to []
-        :type traj_x: list
-        :param traj_y: Y coordinates of trajectory, defaults to []
-        :type traj_y: list
-        """
-
-        assert len(traj_x) == len(traj_y)
-        self.controller.traj_x = traj_x
-        self.controller.traj_y = traj_y
-        self.data_handler.update_traj(traj_x, traj_y)
-
     @property
     def is_finished(self):
-        """Check if pure-pursuit controller is finished or not"""
+        """Check if  controller is finished or not"""
         return self.controller.is_finished
